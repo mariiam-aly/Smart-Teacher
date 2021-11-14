89d2da46ddd10710bcf0c82bbe5636f15ec12a84
mariiam-aly <xmairam.aly@gmail.com>
2021-11-14 23:29:29 +0200

    button changes

--- src/Components/Div1.js
+++ src/Components/Div1.js
@@ -19,8 +19,8 @@ function Div1() {
         <p className="p1 " style={{marginBottom:"0px"}}>  {t("Div1.p1")}</p>
      <h1 id="header1">{t("Div1.header")}</h1>
      <p className="p2 ">{t("Div1.p2")}</p>
-     <a href="#"> <button className="button1" >{t("Div1.btn1")}</button></a>
-     <a href="#"> <button className="button2">{t("Div1.btn2")}</button></a>
+     <a href="#"><button className="button1" >{t("Div1.btn1")}</button></a>
+     <a href="#"><button className="button2">{t("Div1.btn2")}</button></a>
      </div>
      <div  className="col-md  order-first order-md-0">
      <img src={pic} alt=""  className="d-inline-block align-text-top img-fluid"/>
