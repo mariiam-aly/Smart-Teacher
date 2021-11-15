8ffe0e5af4fe98b7645caf3da39147e107aaecff
mariiam-aly <xmairam.aly@gmail.com>
2021-11-15 20:29:36 +0200

    File for all the links

---
+++ src/Components/Links/Links.js
@@ -0,0 +1,8 @@
+const Links={
+
+Smart_Teacher:"https://play.google.com/store/apps/details?id=com.wazos.smart_teacher",
+Smart_Student:"https://play.google.com/store/apps/details?id=com.wazos.smart_student",
+FaceBook:"https://www.google.com/",
+Email:"mailto:webmaster@example.com"
+}
+export default Links;
