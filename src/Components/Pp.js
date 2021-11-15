d91a78527543327c3afe83858aba4d42d8611ec0
mariiam-aly <xmairam.aly@gmail.com>
2021-11-15 15:43:56 +0200

    removed unused vars+ added title to iframe

--- src/Components/Pp.js
+++ src/Components/Pp.js
@@ -3,7 +3,7 @@ import { useTranslation } from "react-i18next";
 import React from 'react';
 function Pp() {
   // FIXME: This is a hack to get the pdf to load + i18n never used
-  const { t, i18n } = useTranslation();
+  const { t} = useTranslation();
   return (<div id="PrivacyPolicy">
     <button type="button" className="Policy" data-bs-toggle="modal" data-bs-target="#PDF">
 
@@ -19,7 +19,7 @@ function Pp() {
             <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div className="modal-body">
-            <iframe src={pdf} width="100%" height="100%"></iframe>
+            <iframe title="Our Privacy Policy" src={pdf} width="100%" height="100%"></iframe>
           </div>
 
         </div>
