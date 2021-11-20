7b6fe648415337d93108a291a834c53ef71276a0
mariiam-aly <xmairam.aly@gmail.com>
2021-11-20 20:10:52 +0200

    added more social apps

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,6 +2,8 @@ import st from './Images/SmartTeacher.svg';
 import companyLogo from './Images/Logo.svg';
 import ss from './Images/SmartStudent.svg';
 import fb from './Images/FaceBook.svg';
+import yt from './Images/Youtube.svg';
+import tel from './Images/Telegram.svg';
 import GooglePlay from './Images/Google Play Badge.png';
 import Links from './Links/Links';
 import { useTranslation } from "react-i18next";
@@ -10,12 +12,12 @@ function Footer() {
    // FIXME: i18n never used
    const { t} = useTranslation();
    return (
-      <div style={{ color: "white" }} className="container " >
+      <div style={{ color: "white",marginLeft:"3em",marginRight:"3em" }}>
          <div style={{ textAlign: "center", paddingTop: "80px" }}>
             <div style={{ paddingBottom: "3em" }} className="row">
 
                <div className="col-lg-3">
-                  <img style={{ minWidth: "200px" }} src={companyLogo} alt="" className="img-fluid" />
+                  <img style={{ minWidth: "200px",maxWidth:"80%" }} src={companyLogo} alt="" className="img-fluid" />
 
                </div>
                <div className="col-lg-6">
@@ -51,9 +53,28 @@ function Footer() {
                <div className="col-lg-3">
                   <div style={{ marginTop: "1.5em" }}>
                      <p className="follow">{t("Footer.p1")}	&mdash;</p>
+<div style={{display:"inline-block",position:"relative",top:"1em"}}>
+                     <div className="socials">
                      <a target="_blank" rel="noopener noreferrer" href={Links.FaceBook}><img src={fb} alt="" className="icon img-fluid pointer" /></a>
 
-                     <p className="pageName">{t("Footer.p2")}</p>
+                     <p className="pageName">{t("Footer.i1")}</p>
+                     </div>
+                     <div  className="socials">
+                     <a target="_blank" rel="noopener noreferrer" href={Links.Telegram_Teacher}><img src={tel} alt="" className="icon img-fluid pointer" /></a>
+
+                     <p className="pageName">{t("Footer.i1")}</p>
+                     </div>
+                     <div  className="socials">
+                     <a target="_blank" rel="noopener noreferrer" href={Links.Telegram_Student}><img src={tel} alt="" className="icon img-fluid pointer" /></a>
+
+                     <p className="pageName">{t("Footer.i3")}</p>
+                     </div>
+                     <div  className="socials">
+                     <a target="_blank" rel="noopener noreferrer" href={Links.Youtube}><img src={yt} alt="" className="icon img-fluid pointer" /></a>
+
+                     <p className="pageName">{t("Footer.i4")}</p>
+                     </div>
+                     </div>
                   </div>
                </div>
 
