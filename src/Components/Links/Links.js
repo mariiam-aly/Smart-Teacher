e3536a0027eff586a43289ba64d669233e575ae7
mariiam-aly <xmairam.aly@gmail.com>
2021-11-20 20:10:02 +0200

    added links for social apps

--- src/Components/Links/Links.js
+++ src/Components/Links/Links.js
@@ -3,6 +3,9 @@ const Links={
 Smart_Teacher:"https://play.google.com/store/apps/details?id=com.wazos.smart_teacher",
 Smart_Student:"https://play.google.com/store/apps/details?id=com.wazos.smart_student",
 FaceBook:"https://www.google.com/",
+Telegram_Teacher:"https://www.google.com/",
+Telegram_Student:"https://www.google.com/",
+Youtube:"https://www.google.com/",
 Email:"mailto:webmaster@example.com"
 }
 export default Links;
